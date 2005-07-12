d51ac1af4decdf1b7b8f44a83262a4c6972bf63b
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2005-07-12 16:00:39 +0000

    Added widget module
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@44 9f1bbd76-14d6-0310-9e99-cbca06d34be2

---
+++ src/widget.c
@@ -0,0 +1,17 @@
+#ifdef _PROTOTYPE_
+
+#include "widget/gul.h"
+#include "widget/base.h"
+#include "widget/theme.h"
+
+#include "widget/layout.h"
+
+#include "widget/box.h"
+#include "widget/container.h"
+#include "widget/scrolling.h"
+#include "widget/list.h"
+#include "widget/mover.h"
+#include "widget/sizer.h"
+#include "widget/text.h"
+
+#endif /* _PROTOTYPE_ */
