705a85b878bce971e44ed6e9a0841abfd018fa09
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2005-07-19 16:04:50 +0000

    added scroll_to, scroll_center, ensure_visible
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@51 9f1bbd76-14d6-0310-9e99-cbca06d34be2

--- src/view.c
+++ src/view.c
@@ -24,3 +24,27 @@ LandView *land_view_new(int x, int y, int w, int h)
     return self;
 }
 
+void land_view_scroll_to(LandView *self, float x, float y)
+{
+    self->scroll_x = x;
+    self->scroll_y = y;
+}
+
+void land_view_scroll_center(LandView *self, float x, float y)
+{
+    self->scroll_x = x - self->w / 2;
+    self->scroll_y = y - self->h / 2;
+}
+
+void land_view_ensure_visible(LandView *self, float x, float y, float bx, float by)
+{
+    if (x - self->scroll_x < bx)
+        self->scroll_x = x - bx;
+    if (x - self->scroll_x > self->w - bx)
+        self->scroll_x = x - self->w + bx;
+    if (y - self->scroll_y < by)
+        self->scroll_y = y - by;
+    if (y - self->scroll_y > self->h - by)
+        self->scroll_y = y - self->h + by;
+}
+
