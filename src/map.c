1f4c72001d69ff7e7c5c3e109246de527aa76661
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2005-07-19 16:04:20 +0000

    implemented multiple layers
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@50 9f1bbd76-14d6-0310-9e99-cbca06d34be2

--- src/map.c
+++ src/map.c
@@ -16,12 +16,25 @@ land_array(LandMap);
 
 void land_map_draw(LandMap *self, LandView *view)
 {
-    land_layer_draw(self->first_layer, view);
+    LandLayer *layer = self->first_layer;
+    while (layer)
+    {
+        land_layer_draw(layer, view);
+        layer = layer->next_in_map;
+    }
 }
 
 void land_map_add_layer(LandMap *map, LandLayer *layer)
 {
-    map->first_layer = layer;
+    if (map->first_layer)
+    {
+        LandLayer *l = map->first_layer;
+        while(l->next_in_map)
+            l = l->next_in_map;
+        l->next_in_map = layer;
+    }
+    else
+        map->first_layer = layer;
 }
 
 LandMap *land_map_new(void)
