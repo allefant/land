46592a450373c372f3af106d623814b277abdbb6
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2006-02-11 20:51:58 +0000

    temporaryly disabled log timestamps for windows
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@153 9f1bbd76-14d6-0310-9e99-cbca06d34be2

--- src/log.c
+++ src/log.c
@@ -5,6 +5,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
+#include <allegro.h>
 #include <time.h>
 #include <sys/time.h>
 
@@ -92,11 +93,15 @@ land_log_msg (char const *template, ...)
     va_start(va_args, template);
     logfile = fopen(logname, "a");
     struct timeval tv;
+#ifndef ALLEGRO_WINDOWS
     gettimeofday(&tv, NULL);
+#endif
     time_t t;
     struct tm tm;
     time(&t);
+#ifndef ALLEGRO_WINDOWS
     gmtime_r(&t, &tm);
+#endif
     fprintf(logfile, "%04d/%02d/%02d %02d:%02d:%02d.%06ld ",
         tm.tm_year + 1900,
         tm.tm_mon + 1,
