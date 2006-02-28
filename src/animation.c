e037e1832887b6df9534c339a9d356d1d5afe500
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2006-02-28 16:11:15 +0000

    started animation module, right now just an array of images
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@196 9f1bbd76-14d6-0310-9e99-cbca06d34be2

--- src/animation.c
+++ src/animation.c
@@ -1,8 +1,27 @@
 #ifdef _PROTOTYPE_
 
+#include "array.h"
+#include "image.h"
+
+typedef struct LandAnimation LandAnimation;
 struct LandAnimation
 {
-    LandList *frames;
+    LandArray *frames;
 };
 
 #endif /* _PROTOTYPE_ */
+
+#include "animation.h"
+
+LandAnimation *land_animation_new(LandArray *frames)
+{
+    LandAnimation *self;
+    land_alloc(self);
+    self->frames = frames;
+    return self;
+}
+
+LandImage *land_animation_get_frame(LandAnimation *self, int i)
+{
+    return land_array_get_nth(self->frames, i);
+}
