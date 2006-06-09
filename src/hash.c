fcc0064d99ddbed07dcaa5ac43259980998c98be
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2006-06-09 19:51:22 +0000

    fixed bug where hash resizing made count go off by one, and added memlog support for hash objects
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@275 9f1bbd76-14d6-0310-9e99-cbca06d34be2

--- src/hash.c
+++ src/hash.c
@@ -18,32 +18,66 @@ struct LandHash
 {
     int count;
     int size;
+    int bits;
     unsigned int (*hash_function)(LandHash *self, char const *thekey);
     LandHashEntry **entries;
 };
+
+
+ LandHash *land_hash_new(void);
+ void land_hash_destroy(LandHash *self);
+
+#ifdef LAND_MEMLOG
+
+#define land_hash_new() land_hash_new_memlog(__FILE__, __LINE__)
+#define land_hash_destroy(x) land_hash_destroy_memlog(x, __FILE__, __LINE__)
+
+#endif
+
+#include "array.h"
 
 #endif /* _PROTOTYPE_ */
 
 #include "hash.h"
 #include "memory.h"
 
+#ifdef LAND_MEMLOG
+
+#undef land_hash_new
+#undef land_hash_destroy
+
+LandHash *land_hash_new_memlog(char const *f, int l)
+{
+    LandHash *hash = land_hash_new();
+    land_memory_add(hash, "hash", 1, f, l);
+    return hash;
+}
+
+void land_hash_destroy_memlog(LandHash *self, char const *f, int l)
+{
+    land_hash_destroy(self);
+    land_memory_remove(self, 1, f, l);
+}
+
+#endif
+
+
 /*
  * FIXME: This is the worst possible hash function.
  */
 static unsigned int hash_function(LandHash *self, char const *thekey)
 {
-    int i, s = 0;
-    unsigned int hash = 0;
+    int i;
+    unsigned int hash = 5381;
     for (i = 0; thekey[i]; i++)
     {
-        hash |= thekey[i] << s;
-        s += 8;
-        s &= 31;
+        unsigned char c = thekey[i];
+        hash = hash * 33 + c;
     }
     return hash & (self->size - 1);
 }
 
-LandHash *land_hash_new(void)
+ LandHash *land_hash_new(void)
 {
     LandHash *self;
     land_alloc(self);
@@ -51,7 +85,7 @@ LandHash *land_hash_new(void)
     return self;
 }
 
-void land_hash_destroy(LandHash *self)
+ void land_hash_destroy(LandHash *self)
 {
     int i;
     for (i = 0; i < self->size; i++)
@@ -67,21 +101,25 @@ void land_hash_destroy(LandHash *self)
             land_free(entry);
         }
     }
+    if (self->entries)
+        land_free(self->entries);
     land_free(self);
 }
 
 void *land_hash_insert(LandHash *self, char const *thekey, void *data)
 {
     int i;
-    self->count++;
-    if (self->count * 2 > self->size)
+
+    /* Need to resize? */
+    if ((self->count + 1) * 2 > self->size)
     {
         int oldsize = self->size;
         LandHashEntry **oldentries = self->entries;
         if (!self->size)
-            self->size = 1;
+            self->bits = 1; /* for first entry, we already want size 2 */
         else
-            self->size *= 2;
+            self->bits++;
+        self->size = 1 << self->bits;
         self->entries = land_calloc(self->size * sizeof *self->entries);
         self->count = 0;
         for (i = 0; i < oldsize; i++)
@@ -98,8 +136,11 @@ void *land_hash_insert(LandHash *self, char const *thekey, void *data)
                 land_free(entry);
             }
         }
-        land_free(oldentries);
+        if (oldentries)
+            land_free(oldentries);
     }
+
+    /* Now there should be ample room to add the new one. */
     i = self->hash_function(self, thekey);
     LandHashEntry *entry = self->entries[i];
     int n = entry ? entry->n + 1 : 1;
@@ -107,11 +148,13 @@ void *land_hash_insert(LandHash *self, char const *thekey, void *data)
     self->entries[i][n - 1].thekey = land_strdup(thekey);
     self->entries[i][n - 1].data = data;
     self->entries[i]->n = n;
+    self->count++;
     return data;
 }
 
 void *land_hash_remove(LandHash *self, char const *thekey)
 {
+    if (!self->size) return NULL;
     int i = self->hash_function(self, thekey);
     if (!self->entries[i]) return NULL;
     int n = self->entries[i]->n;
@@ -135,6 +178,7 @@ void *land_hash_remove(LandHash *self, char const *thekey)
 
 void *land_hash_get(LandHash *self, char const *thekey)
 {
+    if (!self->size) return NULL;
     int i = self->hash_function(self, thekey);
     if (!self->entries[i])
         return NULL;
@@ -145,4 +189,63 @@ void *land_hash_get(LandHash *self, char const *thekey)
             return self->entries[i][j].data;
     }
     return NULL;
+}
+
+LandArray *land_hash_keys(LandHash *hash)
+{
+    LandArray *array = land_array_new();
+    int i;
+    for (i = 0; i < hash->size; i++)
+    {
+        if (hash->entries[i])
+        {
+            int n = hash->entries[i]->n;
+            int j;
+            for (j = 0; j < n; j++)
+                land_array_add_data(&array, hash->entries[i][j].thekey);
+        }
+    }
+    return array;
+}
+
+LandArray *land_hash_data(LandHash *hash)
+{
+    LandArray *array = land_array_new();
+    int i;
+    for (i = 0; i < hash->size; i++)
+    {
+        if (hash->entries[i])
+        {
+            int n = hash->entries[i]->n;
+            int j;
+            for (j = 0; j < n; j++)
+                land_array_add_data(&array, hash->entries[i][j].data);
+        }
+    }
+    return array;
+}
+
+void land_hash_print_stats(LandHash *hash)
+{
+    int i;
+    int u = 0;
+    int c = 0;
+    int l = 0;
+    for (i = 0; i < hash->size; i++)
+    {
+        if (hash->entries[i])
+        {
+            int n = hash->entries[i]->n;
+            if (n > 1)
+                c += n;
+            if (n > l)
+                l = n;
+            u++;
+        }
+    }
+    printf("hash stats: %d/%d[%d%%] full, %d/%d[%d%%] used, %d/%d[%d%%] colliding, longest chain is %d.\n",
+        hash->count, hash->size, hash->size ? 100 * hash->count / hash->size : 0,
+        u, hash->size, hash->size ? 100 * u / hash->size : 0,
+        c, hash->count, hash->count ? 100 * c / hash->count : 0,
+        l);
 }
