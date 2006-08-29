3f9453fc3adfa754c2cf61689d51f233db38a06c
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2006-08-29 21:53:35 +0000

    Added land_use_main macro, I think that's cleaner than land_begin?
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@378 9f1bbd76-14d6-0310-9e99-cbca06d34be2

--- src/land.py
+++ src/land.py
@@ -3,9 +3,26 @@ import main, array, display, runner, random, mouse, keyboard, image
 import exception, font, sprite, map, tilegrid, isometric, sprite
 import log, color, data, memory, widget
 
-macro land_begin() static void _land_main(); int main(int argc, char **argv) {land_argc = argc; land_argv = argv; _land_main(); return 0;} END_OF_MAIN() static void _land_main()
+macro land_begin() static void _land_main(); int main(int argc, char **argv) {
+    land_argc = argc; land_argv = argv; _land_main();
+    return 0;} END_OF_MAIN() static void _land_main()
+    
+macro land_use_main(m) int main(int argc,
+    char **argv) {
+    land_argc = argc;
+    land_argv = argv;
+    m();
+    return 0;} END_OF_MAIN()
 
-macro land_begin_shortcut(w, h, bpp, hz, flags, init, enter, tick, draw, leave, destroy) int main(int argc, char **argv) { land_argc = argc; land_argv = argv; land_init(); shortcut_runner = land_runner_new("shortcut", init, enter, tick, draw, leave, destroy); land_runner_register(shortcut_runner); land_set_display_parameters(w, h, bpp, hz, flags); land_set_initial_runner(shortcut_runner); land_set_frequency(hz); land_main(); return 0; } END_OF_MAIN()
+macro land_begin_shortcut(w, h, bpp, hz, flags, init, enter, tick, draw, leave,
+    destroy) int main(int argc, char **argv) { land_argc = argc;
+    land_argv = argv; land_init();
+    shortcut_runner = land_runner_new("shortcut", init, enter, tick, draw,
+        leave, destroy);
+    land_runner_register(shortcut_runner);
+    land_set_display_parameters(w, h, bpp, hz, flags);
+    land_set_initial_runner(shortcut_runner);
+    land_set_frequency(hz); land_main(); return 0; } END_OF_MAIN()
 
 global int land_argc
 global char **land_argv
