cf38636216503dfe89755b98d976ab9f9f4d437f
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2006-10-06 13:05:36 +0000

    Improved sizer widget.
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@419 9f1bbd76-14d6-0310-9e99-cbca06d34be2

--- src/widget/sizer.py
+++ src/widget/sizer.py
@@ -12,6 +12,18 @@ LandWidgetInterface *land_widget_sizer_interface = NULL
 
 def land_widget_sizer_draw(LandWidget *self):
     land_widget_theme_draw(self)
+    float x = self->box.x + self->box.il;
+    float y = self->box.y + self->box.it;
+    float x_ = self->box.x + self->box.w - self->box.ir;
+    float y_ = self->box.y + self->box.h - self->box.ib;
+    land_color(1, 1, 1, 0.5)
+    land_line(x_, y, x, y_)
+    land_line(x_, y + 3, x + 3, y_)
+    land_line(x_, y + 6, x + 6, y_)
+    land_color(0, 0, 0, 0.5)
+    land_line(x_, y + 1, x + 1, y_)
+    land_line(x_, y + 4, x + 4, y_)
+    land_line(x_, y + 7, x + 7, y_)
 
 def land_widget_sizer_mouse_tick(LandWidget *super):
     LandWidgetSizer *self = LAND_WIDGET_SIZER(super)
@@ -31,18 +43,26 @@ def land_widget_sizer_mouse_tick(LandWidget *super):
             land_mouse_y() - b + self->drag_y)
 
 
-LandWidget *def land_widget_sizer_new(LandWidget *parent, int x, int y, int w, int h):
+LandWidget *def land_widget_sizer_new(LandWidget *parent, int x, y, w, h):
     LandWidgetSizer *self
     
     land_widget_sizer_interface_initialize()
+    
+    # FIXME: inhibit layout changes until our own vt is set
 
     land_alloc(self)
     LandWidget *super = &self->super
     land_widget_base_initialize(super, parent, x, y, w, h)
     super->vt = land_widget_sizer_interface
+
+    land_widget_layout_set_shrinking(super, 1, 1)
+    land_widget_theme_layout_border(super)
+    if parent: land_widget_layout(parent)
+    
     # by default, size the parent. 
     self->target = parent
     self->dragged = 0
+
     return super
 
 def land_widget_sizer_interface_initialize():
