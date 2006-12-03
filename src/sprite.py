5f355653b5f5a90517717944b25225159e15f444
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2006-12-03 16:16:01 +0000

    Added new method land_sprites_grid_clear
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@447 9f1bbd76-14d6-0310-9e99-cbca06d34be2

--- src/sprite.py
+++ src/sprite.py
@@ -69,8 +69,8 @@ class LandSpriteAnimated:
     float r, g, b, a
 
 class LandSpritesGrid:
-    LandGrid super; # 2D-array of sprite lists
-    LandList **sprites
+    LandGrid super
+    LandList **sprites # 2D-array of sprite lists
 
     # Maximum sprite extents. This is the maximum values of the x/y/w/h of
     # the sprite type above, relative to the sprite position.
@@ -93,27 +93,29 @@ LandGrid *def land_sprites_grid_new(int cell_w, cell_h, x_cells, y_cells):
 
     return &self->super
 
-
-# Deletes a sprites grid, and all the sprites in it. If you need to keep
-# a sprite, first remove it from the grid.
-def land_sprites_grid_del(LandGrid *super):
-
+def land_sprites_grid_clear(LandGrid *super):
+    """
+    Removes all sprites from a grid, and destroys them all.
+    """
     LandSpritesGrid *self = LAND_SPRITES_GRID(super)
     int j
     for j = 0; j < super->x_cells * super->y_cells; j++:
-
         if self->sprites[j]:
-
             # Destroy all sprites in the list.
             LandListItem *i
             for i = self->sprites[j]->first; i; i = i->next:
-
                 LandSprite *s = i->data
                 land_sprite_del(s)
-
             land_list_destroy(self->sprites[j])
-
-
+            self->sprites[j] = None
+        
+def land_sprites_grid_del(LandGrid *super):
+    """
+    Deletes a sprites grid, and all the sprites in it. If you need to keep
+    a sprite, first remove it from the grid.
+    """
+    land_sprites_grid_clear(super)
+    LandSpritesGrid *self = LAND_SPRITES_GRID(super)
     land_free(self->sprites)
     land_free(self)
 
@@ -209,44 +211,45 @@ def land_sprite_animated_destroy(LandSprite *sprite):
     pass
 
 
-# Destroys a sprite. This will not remove its reference from a grid in case it
-# is inside one - so only use this if you know what you are doing.
 def land_sprite_del(LandSprite *self):
-
+    """
+    Destroys a sprite. This will not remove its reference from a grid in case it
+    is inside one - so only use this if you know what you are doing.
+    """
     if self->type->destroy: self->type->destroy(self)
     land_free(self)
 
 
 def land_sprite_destroy(LandSprite *self):
-
+    """Same as land_sprite_del."""
     land_sprite_del(self)
-
-
 
 def land_sprite_show(LandSprite *self):
     pass
 
-
-
 def land_sprite_hide(int sprite, int grid):
     pass
 
-# Given two sprites who have a type LandSpriteTypeImage, do a pixel overlap
-# test, and return 0 if they don't overlap.
 int def land_sprite_overlap_pixelperfect(LandSprite *self, LandSprite *other):
+    """
+    Given two sprites who have a type LandSpriteTypeImage, do a pixel overlap
+    test, and return 0 if they don't overlap.
+    """
     return land_image_overlaps(LAND_SPRITE_TYPE_IMAGE(self->type)->image,
     self->x, self->y, self->angle,
     LAND_SPRITE_TYPE_IMAGE(other->type)->image,
     other->x, other->y, other->angle)
 
-# Return a list of all sprites overlapping the sprite in the given grid.
-# The sprite itself is not returned.
-# Note that this only works if all sprites in the grid have compatible
-# overlapping, e.g. if the sprite does a pixel-overlap test, then all other
-# sprites must have a pixel mask, or if it does a bounding circle test, then
-# all other sprites must have a bounding circle.
-LandList *def land_sprites_grid_overlap(LandSprite *self, LandGrid
-    *sprites_grid):
+LandList *def land_sprites_grid_overlap(LandSprite *self,
+    LandGrid *sprites_grid):
+    """
+    Return a list of all sprites overlapping the sprite in the given grid.
+    The sprite itself is not returned.
+    Note that this only works if all sprites in the grid have compatible
+    overlapping, e.g. if the sprite does a pixel-overlap test, then all other
+    sprites must have a pixel mask, or if it does a bounding circle test, then
+    all other sprites must have a bounding circle, and so on.
+    """
 
     LandSpritesGrid *grid = LAND_SPRITES_GRID(sprites_grid)
     LandList *retlist = NULL
@@ -281,11 +284,13 @@ LandList *def land_sprites_grid_overlap(LandSprite *self, LandGrid
 
     return retlist
 
-
-# Return a list of all sprites in the grid, which are inside the given circle
-# (in pixels).
-LandList *def land_sprites_grid_get_circle(LandGrid *sprites_grid, float x, float y,
+LandList *def land_sprites_grid_get_circle(LandGrid *sprites_grid, float x, y,
     float radius):
+    """
+    Return a list of all sprites in the grid, whose position is inside the given
+    circle (in pixels). The size of the sprite is currently ignored, only its
+    point position is used.
+    """
 
     LandSpritesGrid *grid = LAND_SPRITES_GRID(sprites_grid)
     LandList *retlist = NULL
@@ -321,15 +326,14 @@ LandList *def land_sprites_grid_get_circle(LandGrid *sprites_grid, float x, floa
 
                     item = item->next
 
-
-
-
     return retlist
 
-
-# Return a list of all sprites in the given rectangle.
 LandList *def land_sprites_grid_get_rectangle(LandGrid *sprites_grid,
-    float l, float t, float r, float b):
+    float l, t, r, b):
+    """
+    Return a list of all sprites in the given rectangle. All the sprites who
+    are in one of the grid cells overlapped by the rectangle are returned.
+    """
 
     LandSpritesGrid *grid = LAND_SPRITES_GRID(sprites_grid)
     LandList *retlist = NULL
@@ -356,17 +360,12 @@ LandList *def land_sprites_grid_get_rectangle(LandGrid *sprites_grid,
                     land_add_list_data(&retlist, other)
                     item = item->next
 
-
-
-
     return retlist
-
-
 
 # Return a list of all sprites in the given view and overlap. The overlap
 # increases when negative for t and l and positive for r and l.
-LandList *def land_sprites_grid_get_in_view(LandGrid *sprites_grid, LandView *view,
-    float l, float t, float r, float b):
+LandList *def land_sprites_grid_get_in_view(LandGrid *sprites_grid,
+    LandView *view, float l, t, r, b):
 
     l += view->scroll_x + view->x
     t += view->scroll_y + view->y
@@ -374,9 +373,10 @@ LandList *def land_sprites_grid_get_in_view(LandGrid *sprites_grid, LandView *vi
     b += view->scroll_y + view->y + view->h
     return land_sprites_grid_get_rectangle(sprites_grid, l, t, r, b)
 
-
 def grid_place(LandSprite *self, LandSpritesGrid *grid):
-
+    """
+    Place a sprite into a grid.
+    """
     int tx = self->x / grid->super.cell_w
     int ty = self->y / grid->super.cell_h
 
@@ -397,7 +397,9 @@ def grid_place(LandSprite *self, LandSpritesGrid *grid):
         self)
 
 def grid_unplace(LandSprite *self, LandSpritesGrid *grid):
-
+    """
+    Remove a sprite from a grid.
+    """
     int tx = self->x / grid->super.cell_w
     int ty = self->y / grid->super.cell_h
 
@@ -409,21 +411,26 @@ def grid_unplace(LandSprite *self, LandSpritesGrid *grid):
     land_remove_list_data(&grid->sprites[ty * grid->super.x_cells + tx],
         self)
 
-
 def land_sprite_remove_from_grid(LandSprite *self, LandGrid *grid):
-
+    """
+    Remove a sprite from a grid.
+    """
     grid_unplace(self, LAND_SPRITES_GRID(grid))
-
 
 def land_sprite_place_into_grid(LandSprite *self, LandGrid *grid, float
         x, float y):
+    """
+    Place a sprite into a grid.
+    """
     self->x = x
     self->y = y
     grid_place(self, (LandSpritesGrid *)grid)
 
 
 def land_sprite_move(LandSprite *self, LandGrid *grid, float x, float y):
-
+    """
+    Move a sprite by the given amount of pixels
+    """
     grid_unplace(self, (LandSpritesGrid *)grid)
     self->x += x
     if self->x < 0: self->x = 0
@@ -437,6 +444,11 @@ def land_sprite_move(LandSprite *self, LandGrid *grid, float x, float y):
 
     grid_place(self, (LandSpritesGrid *)grid)
 
+def land_sprite_move_to(LandSprite *self, LandGrid *grid, float x, float y):
+    """
+    Move a sprite to the given position in pixels.
+    """
+    land_sprite_move(self, grid, x - self->x, y - self->y)
 
 def land_sprites_grid_draw_cell(LandSpritesGrid *self, LandView *view,
     int cell_x, cell_y, float pixel_x, pixel_y):
@@ -530,11 +542,13 @@ LandSpriteType *def land_spritetype_image_new(LandImage *image):
     land_spritetype_image_initialize(self, image)
     return LAND_SPRITE_TYPE(self)
 
-
-# Create a new animation sprite type with the given animation. The image is
-# used for collision detection.
 LandSpriteType *def land_spritetype_animation_new(LandAnimation *animation,
     LandImage *image):
+    """
+    Create a new animation sprite type with the given animation. The image is
+    used for collision detection.
+    If no image is given, the first frame of the animation is used instead.
+    """
 
     LandSpriteTypeAnimation *self
     land_alloc(self)
