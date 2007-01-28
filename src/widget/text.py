439120fd9609e9a7adfc55f459a25306b8f6e825
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2007-01-28 11:49:37 +0000

    Added scrolling text widget.
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@515 9f1bbd76-14d6-0310-9e99-cbca06d34be2

---
+++ src/widget/text.py
@@ -0,0 +1,66 @@
+import scrolling
+
+class LandWidgetScrollingText:
+    """
+    A scrolling text widget. You can also put text into normal scrolling widget
+    to get the same effect, but this widget already handles some extra stuff
+    like word wrapping. (For non-wrapped text it is exactly the same as a
+    normal scrolling widget with a text child.)
+    """
+    LandWidgetScrolling super
+
+static import land
+
+global LandWidgetInterface *land_widget_scrolling_text_interface
+
+def land_widget_scrolling_text_initialize(LandWidget *base,
+    LandWidget *parent, char const *text, int wordwrap,
+    int x, int y, int w, int h):
+    
+    #FIXME: Inhibit layout changes of parent, they make no sense before we
+    # set the layout below
+
+    land_widget_scrolling_initialize(base, parent, x, y, w, h)
+
+    land_widget_scrolling_text_interface_initialize()
+
+    base->vt = land_widget_scrolling_text_interface
+
+    land_widget_theme_initialize(base)
+
+    LandWidget *container = land_widget_scrolling_get_container(base)
+    float cx, cy, cw, ch
+    land_widget_inner(container, &cx, &cy, &cw, &ch)
+    land_widget_text_new(base, text, 1, cx, cy, cw, ch)
+
+LandWidget *def land_widget_scrolling_text_new(LandWidget *parent,
+    char const *text, int wordwrap, int x, int y, int w, int h):
+    LandWidgetScrollingText *self
+    land_alloc(self)
+    LandWidget *widget = (LandWidget *)self
+
+    land_widget_scrolling_text_initialize(widget,
+        parent, text, wordwrap, x, y, w, h)
+
+    return widget
+
+def land_widget_scrolling_text_size(LandWidget *widget, float dx, float dy):
+    if not (dx or dy): return
+    LandWidget *container = land_widget_scrolling_get_container(widget)
+    float cx, cy, cw, ch
+    land_widget_layout(widget)
+    land_widget_inner(container, &cx, &cy, &cw, &ch)
+    LandWidget *button = land_widget_container_child(container)
+    land_widget_size(button, cw - button->box.w, ch - button->box.h)
+    land_widget_button_wordwrap(button, LAND_WIDGET_BUTTON(button)->wordwrap)
+
+    land_widget_scrolling_size(widget, dx, dy)
+
+def land_widget_scrolling_text_interface_initialize():
+    if land_widget_scrolling_text_interface: return
+    land_widget_scrolling_interface_initialize()
+    land_widget_scrolling_text_interface = land_widget_copy_interface(
+        land_widget_scrolling_interface, "scrolling")
+    land_widget_scrolling_text_interface->id |= LAND_WIDGET_ID_SCROLLING_TEXT
+    land_widget_scrolling_text_interface->size = land_widget_scrolling_text_size
+
