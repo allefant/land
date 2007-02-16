596d7d8bfbf14eef8b5732edc04a471d39ba4ecc
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2007-02-16 11:28:53 +0000

    Added zlib compression support.
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@578 9f1bbd76-14d6-0310-9e99-cbca06d34be2

--- src/buffer.py
+++ src/buffer.py
@@ -1,4 +1,4 @@
-static import global stdlib, string
+static import global stdio, stdlib, string, zlib
 static import memory, global allegro
 
 class LandBuffer:
@@ -21,7 +21,7 @@ def land_buffer_add(LandBuffer *self, char const *buffer, int n):
         if not self->size: self->size = 1
         while self->size < self->n:
             self->size *= 2
-        self->buffer = realloc(self->buffer, self->size)
+        self->buffer = land_realloc(self->buffer, self->size)
     memcpy(self->buffer + self->n - n, buffer, n)
 
 def land_buffer_cat(LandBuffer *self, char const *string):
@@ -68,3 +68,26 @@ LandBuffer *def land_buffer_read_from_file(char const *filename):
         land_buffer_add(self, (char *)&c, 1)
     pack_fclose(pf)
     return self
+
+def land_buffer_compress(LandBuffer *self):
+    uLongf destlen = self->n * 1.1 + 12
+    Bytef *dest = land_malloc(4 + destlen)
+    *((uint32_t *)dest) = self->n
+    compress(dest + 4, &destlen, (void *)self->buffer, self->n)
+    dest = land_realloc(dest, 4 + destlen)
+    land_free(self->buffer)
+    self->buffer = (void *)dest
+    self->size = self->n = 4 + destlen
+
+def land_buffer_decompress(LandBuffer *self):
+    uLongf destlen = *((uint32_t *)self->buffer)
+    Bytef *dest = land_malloc(destlen)
+    uncompress(dest, &destlen, (void *)(4 + self->buffer), self->n)
+    land_free(self->buffer)
+    self->buffer = (void *)dest
+    self->size = self->n = destlen
+
+int def land_buffer_compare(LandBuffer *self, *other):
+    if self->n < other->n: return -1
+    if self->n > other->n: return 1
+    return memcmp(self->buffer, other->buffer, self->n)
