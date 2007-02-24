c24e2d88a3c5dd3f4c7fc3ce1d5f9989089bf9a1
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2007-02-24 14:38:02 +0000

    Added textview example
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@592 9f1bbd76-14d6-0310-9e99-cbca06d34be2

---
+++ examples/textview/src/textview.py
@@ -0,0 +1,52 @@
+import global land
+
+LandWidget *desktop
+LandWidgetTheme *theme
+LandWidget *scrolling, *contents
+
+def create_dialog():
+    land_font_load("../../data/galaxy.ttf", 12)
+    theme = land_widget_theme_new("../../data/classic.cfg")
+    land_widget_theme_set_default(theme)
+    desktop = land_widget_board_new(None, 0, 0, 640, 480)
+    land_widget_reference(desktop)
+
+    char *text = land_read_text("GPL-2")
+
+    LandWidget *window =  land_widget_vbox_new(desktop, 100, 100, 4, 4)
+    LandWidget *mover = land_widget_mover_new(window, "GPL-2", 0, 0, 4, 4)
+
+    scrolling = land_widget_scrolling_text_new(window,
+        text, 2,
+        100, 100, 200, 200)
+    land_widget_scrolling_autohide(scrolling, 1, 1, 2)
+
+    LandWidget *empty = land_widget_scrolling_get_empty(scrolling)
+    LandWidget *sizer = land_widget_sizer_new(empty, 0, 0, 10, 10)
+    land_widget_layout_set_expanding(sizer, 1, 1)
+    land_widget_sizer_set_target(sizer, scrolling)
+
+def init(LandRunner *r):
+    create_dialog()
+
+def tick(LandRunner *r):
+    if land_closebutton(): land_quit()
+    if land_key_pressed(KEY_ESC): land_quit()
+
+    int kx = 0, ky = 0
+    if land_key(KEY_LEFT): kx -= 1
+    if land_key(KEY_RIGHT): kx += 1
+    if land_key(KEY_UP): ky -= 1
+    if land_key(KEY_DOWN): ky += 1
+
+    if kx or ky:
+        land_widget_scrolling_scroll(scrolling, kx, ky)
+
+    land_widget_tick(desktop)
+
+def draw(LandRunner *r):
+    land_widget_draw(desktop)
+
+land_begin_shortcut(640, 480, 0, 60,
+    LAND_OPENGL | LAND_WINDOWED,
+    init, None, tick, draw, None, None)
