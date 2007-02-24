19abd1438b838269152817c6ceb06c303e7f01d6
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2007-02-24 14:40:42 +0000

    added clip example
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@593 9f1bbd76-14d6-0310-9e99-cbca06d34be2

---
+++ examples/clip/src/clip.py
@@ -0,0 +1,41 @@
+import global land
+
+LandImage *image
+
+static def game_init(LandRunner *self):
+    image = land_image_new(16, 16)
+    land_set_image_display(image)
+    land_clear(0.5, 0.5, 0.5, 1)
+    land_color(1, 1, 0, 1)
+    land_line(0, 0, 16, 16)
+    land_unset_image_display()
+    
+    glEnable(GL_POLYGON_SMOOTH)
+
+static def game_tick(LandRunner *self):
+    if land_key(KEY_ESC) || land_closebutton():
+        land_quit()
+
+static def game_draw(LandRunner *self):
+    land_clip(0, 0, 640, 480)
+    land_color(1, 1, 0, 1)
+    land_filled_rectangle(0, 0, 640, 480)
+    land_color(0.5, 0.5, 0.5, 1)
+    land_filled_rectangle(1, 1, 639, 479)
+    
+    int i, j
+    for j = 0; j < 15; j++:
+        for i = 0; i < 15; i++:
+            float x = 2 + i * 20
+            float y = 2 + j * 20
+            float clip = x + 8 + i * 0.1
+            x += j * 0.1
+            land_clip(clip, 0, 640, 480)
+            land_image_draw_tinted(image, x, y, 1, 0, 0, 0.8)
+            land_clip(0, 0, clip, 480)
+            land_image_draw_tinted(image, x, y, 0, 1, 0, 0.8)
+
+
+
+land_begin_shortcut(640, 480, 32, 120, LAND_WINDOWED | LAND_OPENGL,
+    game_init, NULL, game_tick, game_draw, NULL, NULL)
