9ea4d8e4ea28ef6bee72362a26d2c06a2c66b99f
elias <elias@9f1bbd76-14d6-0310-9e99-cbca06d34be2>
2008-01-12 23:03:05 +0000

    Added a few convenience function to the animation module.
    git-svn-id: https://lland.svn.sourceforge.net/svnroot/lland/land@729 9f1bbd76-14d6-0310-9e99-cbca06d34be2

--- src/animation.py
+++ src/animation.py
@@ -25,3 +25,29 @@ def land_animation_destroy(LandAnimation *self):
 
 LandImage *def land_animation_get_frame(LandAnimation *self, int i):
     return land_array_get_nth(self->frames, i)
+
+LandAnimation *def land_animation_load_cb(char const *pattern,
+    void (*cb)(LandImage *image, void *data), void *data):
+    """
+    Create a new animation from all files matching the pattern, sorted
+    alphabetically. The callback function, if present, is called on each
+    frame.
+    """
+    LandArray *pics = land_load_images_cb(pattern, cb, data)
+    if not pics: return None
+    return land_animation_new(pics)
+
+def land_animation_draw_frame(LandAnimation *self, int i,
+    float x, y):
+    LandImage *frame = land_animation_get_frame(self, i)
+    land_image_draw(frame, x, y)
+
+def land_animation_draw_frame_rotated(LandAnimation *self, int i,
+    float x, y, angle):
+    LandImage *frame = land_animation_get_frame(self, i)
+    land_image_draw_rotated(frame, x, y, angle)
+
+def land_animation_draw_frame_scaled_rotated(LandAnimation *self, int i,
+    float x, y, xs, ys, angle):
+    LandImage *frame = land_animation_get_frame(self, i)
+    land_image_draw_scaled_rotated(frame, x, y, xs, ys, angle)
