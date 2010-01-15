ed48b41e8e14494a0d88b04e798605975b799d79
Debian User <elias@debian.(none)>
2010-01-15 17:19:35 +0100

    added some test features to scrolling3 example

--- examples/scrolling3/src/scrolling3.py
+++ examples/scrolling3/src/scrolling3.py
@@ -2,25 +2,28 @@ import global land/land
 
 LandWidget *desktop
 
+LandWidget *window
+LandWidget *scrolling
+
 static def game_init(LandRunner *self):
     land_font_load("../../data/galaxy.ttf", 12)
 
-    land_widget_theme_set_default(land_widget_theme_new("../../data/classic.cfg"))
+    land_widget_theme_set_default(land_widget_theme_new("../../data/green.cfg"))
     desktop = land_widget_board_new(NULL, 0, 0, 640, 480)
     land_widget_reference(desktop)
     
-    LandWidget *window = land_widget_panel_new(desktop, 50, 50, 540, 380)
+    window = land_widget_panel_new(desktop, 50, 50, 350, 350)
     LandWidget *vbox = land_widget_vbox_new(window, 0, 0, 10, 10)
     LandWidget *mover = land_widget_mover_new(vbox, "Scrolling3", 0, 0, 10, 10)
     land_widget_mover_set_target(mover, window)
-    LandWidget *scrolling = land_widget_scrolling_new(vbox, 0, 0, 10, 10)
+    scrolling = land_widget_scrolling_new(vbox, 0, 0, 10, 10)
     land_widget_scrolling_autohide(scrolling, 1, 1, 2)
     LandWidget *hbox = land_widget_hbox_new(vbox, 0, 0, 10, 10)
-    LandWidget *box = land_widget_box_new(hbox, 0, 0, 10, 10)
+    land_widget_box_new(hbox, 0, 0, 10, 10)
     land_widget_layout_set_shrinking(hbox, 1, 1)
     LandWidget *sizer = land_widget_sizer_new(hbox, 3, 0, 0, 10, 10)
     land_widget_sizer_set_target(sizer, window)
-    LandWidget *mainw = land_widget_box_new(scrolling, 0, 0, 256, 256)
+    land_widget_box_new(scrolling, 0, 0, 256, 256)
 
 static def debug(LandWidget *w):
     land_print("* %s: %d %d %d %d %s]", w->vt->name,
@@ -42,13 +45,21 @@ static def debug(LandWidget *w):
 static def game_tick(LandRunner *self):
     if land_key_pressed(LandKeyEscape) || land_closebutton():
         land_quit()
+    
+    if land_key_pressed('u'):
+        land_widget_scrolling_update(scrolling)
+    
+    if land_key_pressed(LandKeyLeft): land_widget_resize(window, -1, 0)
+    if land_key_pressed(LandKeyRight): land_widget_resize(window, 1, 0)
+    if land_key_pressed(LandKeyUp): land_widget_resize(window, 0, -1)
+    if land_key_pressed(LandKeyDown): land_widget_resize(window, 0, 1)
 
     land_widget_tick(desktop)
 
 static def game_draw(LandRunner *self):
     land_widget_draw(desktop)
     
-    land_text_pos(300, 50)
+    land_text_pos(300, 10)
     land_color(0, 0, 0, 0.75)
     debug(desktop)
 
