8fa33da9231b39de39ef8b97229b866be5ace354
Elias Pschernig <elias@users.sourceforge.net>
2012-08-16 21:20:31 +0200

    Added land_thread_run function.

--- src/allegro5/a5_thread.py
+++ src/allegro5/a5_thread.py
@@ -1,10 +1,37 @@
 import land.thread
+import land.mem
 static import global allegro5.allegro5
 
-void *def proc(void *cb):
-    void (*p)(void) = cb
-    p()
+static class PlatformThread:
+    LandThread super
+    ALLEGRO_THREAD *a5
+
+static void *def proc(void *data):
+    LandThread *t = data
+    t->cb(t->data)
+    land_free(t)
     return NULL
 
-def platform_thread_run(void (*cb)(void)):
-    al_run_detached_thread(proc, cb)
+def platform_thread_run(void (*cb)(void *), void *data):
+    LandThread *t; land_alloc(t)
+    t->cb = cb
+    t->data = data
+    al_run_detached_thread(proc, t)
+
+static void *def aproc(ALLEGRO_THREAD *thread, void *arg):
+    LandThread *t = arg
+    t->cb(t->data)
+    return NULL
+
+LandThread *def platform_thread_new(void (*cb)(void *data), void *data):
+    PlatformThread *t; land_alloc(t)
+    t->super.cb = cb
+    t->super.data = data
+    t->a5 = al_create_thread(aproc, t)
+    al_start_thread(t->a5)
+    return &t->super
+
+def platform_thread_destroy(LandThread *self):
+    PlatformThread *t = (void *)self
+    al_destroy_thread(t->a5)
+    land_free(self)
--- src/thread.py
+++ src/thread.py
@@ -1,3 +1,14 @@
 static import allegro5/a5_thread
-def land_thread_run(void (*cb)(void)):
-    platform_thread_run(cb)
+
+class LandThread:
+    void *data
+    void (*cb)(void *data)
+
+def land_thread_run(void (*cb)(void *data), void *data):
+    platform_thread_run(cb, data)
+
+LandThread *def land_thread_new(void (*cb)(void *data), void *data):
+    return platform_thread_new(cb, data)
+
+def land_thread_destroy(LandThread *t):
+    platform_thread_destroy(t)
