093501388e801c477afbcb7e5ef174788581ee3d
Elias Pschernig <elias@users.sourceforge.net>
2012-08-23 23:29:44 +0200

    Added land_opengl_error function.

---
+++ src/openglutil.py
@@ -0,0 +1,16 @@
+import allegro5.a5_opengl
+
+char const *def land_opengl_error():
+    GLenum e = glGetError()
+***scramble
+for e in ["GL_NO_ERROR",
+    "GL_INVALID_ENUM",
+    "GL_INVALID_VALUE",
+    "GL_INVALID_OPERATION",
+    "GL_INVALID_FRAMEBUFFER_OPERATION",
+    "GL_OUT_OF_MEMORY"]:
+        parse("""
+    if e == {e}: return "{e}"
+""".format(e = e))
+***
+    return "unknown"
