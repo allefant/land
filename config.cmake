ccb41abd589500f491ea6193742e6bba74bb8091
Elias Pschernig <elias@users.sourceforge.net>
2021-05-19 10:04:13 -0400

    add some example data files

---
+++ config.cmake
@@ -0,0 +1,2 @@
+#cmakedefine LAND_HAVE_YAML_H
+#cmakedefine LAND_HAVE_EXECINFO_H
---
+++ data/flowers/render.sh
@@ -0,0 +1,7 @@
+cd /home/elias/prog/land/fruits
+./fruits -o ../land/data/flowers -d 1 bluebells
+./fruits -o ../land/data/flowers -d 1 hepatica
+./fruits -o ../land/data/flowers -d 1 primrose
+./fruits -o ../land/data/flowers/hd -d 2 bluebells
+./fruits -o ../land/data/flowers/hd -d 2 hepatica
+./fruits -o ../land/data/flowers/hd -d 2 primrose
